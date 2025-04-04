9df21eab3ab692d64a6879d09b34cb2a9fd9393e
Atif Aiman <alserembani94@gmail.com>
2025-04-04 17:40:51 +0800

    Removed old easter egg

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,30 +4,24 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
-import { EasterEggProvider, useEasterEgg } from '@/hooks/useEasterEgg';
 import { useEffect, useState } from 'react';
-import DragonDen from '@/components/sections/DragonDen';
 
 const queryClient = new QueryClient();
 
 export default function App(props: AppProps) {
   
   return (
-    <EasterEggProvider>
       <QueryClientProvider client={queryClient}>
           <ElevatedApp {...props} />
       </QueryClientProvider>
-    </EasterEggProvider>
   )
 }
 
 const ElevatedApp = ({ Component, pageProps }: AppProps) => {
-  const { isOpenDen } = useEasterEgg();
 
   return (
     <>
       <Component {...pageProps} />
-      { isOpenDen && <DragonDen /> }
     </>
   )
 }
